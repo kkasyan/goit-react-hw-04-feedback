18b47e097918eacf0cb811d9c344c1842cfb492c
kkasyan <104075234+kkasyan@users.noreply.github.com>
2023-02-02 20:30:13 +0200

    refactored the task with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './app.module.css';
 
 import { Statistics } from './Statistics/Statistics';
@@ -6,67 +6,52 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { NotificationMessage } from './NotificationMessage/NotificationMessage';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  elements = Object.keys(this.state);
+  const elements = Object.keys(state);
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
+  const { bad, good, neutral } = state;
+  const total = good + neutral + bad;
 
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
     if (!total) {
       return 0;
     }
+
     const result = (good / total) * 100;
     return Number(result.toFixed(2));
-  }
+  };
 
-  onLeaveFeedback = rateName => {
-    this.setState(prevState => {
+  const onLeaveFeedback = rateName => {
+    setState(prevState => {
       const value = prevState[rateName];
 
       return {
+        ...prevState,
         [rateName]: value + 1,
       };
     });
   };
 
-  render() {
-    const { bad, good, neutral } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <div className={css.wrap}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.elements}
-            onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <div className={css.wrap}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={elements} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {!total ? (
+          <NotificationMessage message={'There is no feedback!'} />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {!total ? (
-            <NotificationMessage message={'There is no feedback!'} />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        )}
+      </Section>
+    </div>
+  );
+};
